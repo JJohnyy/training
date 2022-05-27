55a9c41113653eb18e76994255b66984e5ff579b
JJohnyy <jan.jasa@email.cz>
2022-05-27 06:07:01 +0000

    .

--- blog/admin.py
+++ blog/admin.py
@@ -3,24 +3,22 @@ from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
-# Register your models here.
-
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
     list_display = ('title', 'slug', 'status', 'created_on')
-    search_fields = ('title', 'content')
-    prepopulated_fields = {'slug': ('title',)}
+    search_fields = ['title', 'content']
     list_filter = ('status', 'created_on')
-    summernote_fields = ('content')
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
+
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-
     list_display = ('name', 'body', 'post', 'created_on', 'approved')
     list_filter = ('approved', 'created_on')
     search_fields = ('name', 'email', 'body')
     actions = ['approve_comments']
 
     def approve_comments(self, request, queryset):
-        queryset.update(approve=True)
+        queryset.update(approved=True)
